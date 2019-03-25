56794a935ff074e276c6460587a3fbdba92d0d3d
unipanic <wolf@dorkbrigade.com>
2019-03-25 15:16:31 -0700

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,23 @@
-
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+        self.following = []
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        # Cambie el nombre a get_timeline
+        posts = []
+        for user in self.following:
+            posts += user.posts
+
+        return sorted(posts, key=lambda p: p.timestamp, reverse=False)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
